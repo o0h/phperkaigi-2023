5a26b124c3d0d4d36e9b5e579e2d92b2b6baddc4
o0h <dev@o0h.in>
2023-03-23 14:04:20 +0900

    写経アプリの実行環境

--- perfect-php-shakyo/.runtime/database/shared_files/docker-entrypoint-initdb.d/00_create-test.sql
+++ perfect-php-shakyo/.runtime/database/shared_files/docker-entrypoint-initdb.d/00_create-test.sql
@@ -1,1 +1,38 @@
---- do some
+CREATE TABLE user
+(
+    id         INTEGER auto_increment,
+    user_name  VARCHAR(20) NOT NULL,
+    password   VARCHAR(40) NOT NULL,
+    created_at DATETIME,
+    PRIMARY KEY (id),
+    UNIQUE user_name_index (user_name)
+)
+    engine = innodb;
+
+CREATE TABLE following
+(
+    user_id      INTEGER,
+    following_id INTEGER,
+    PRIMARY KEY (user_id, following_id)
+)
+    engine = innodb;
+
+CREATE TABLE status
+(
+    id         INTEGER auto_increment,
+    user_id    INTEGER NOT NULL,
+    body       VARCHAR(255),
+    created_at DATETIME,
+    PRIMARY KEY (id),
+    INDEX user_id_index (user_id)
+)
+    engine = innodb;
+
+ALTER TABLE following
+    ADD FOREIGN KEY (user_id) REFERENCES user (id);
+
+ALTER TABLE following
+    ADD FOREIGN KEY (following_id) REFERENCES user (id);
+
+ALTER TABLE status
+    ADD FOREIGN KEY (user_id) REFERENCES user (id);
